e49c94060418448d4729631f314af4c55f122363
Muhammadaziz <muhammdazizgulomov9@gmail.com>
2025-03-13 22:55:17 +0500

    fixed

--- src/api/debts/dto/updateDebt-dto.ts
+++ src/api/debts/dto/updateDebt-dto.ts
@@ -1,4 +1,4 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import {
   IsDecimal,
   IsEnum,
@@ -7,46 +7,54 @@ import {
   IsDateString,
 } from 'class-validator';
 import { DebtPeriod } from 'src/common/enum';
+import { DebtDto } from './createDebt-dto';
 
-export class UpdateDebtDto {
-  @ApiPropertyOptional({
-    description: 'The date when the debt was created',
-    example: '2025-01-22',
-  })
-  @IsOptional()
-  @IsDateString()
-  debt_date?: string;
+// export class UpdateDebtDto {
+//   @ApiPropertyOptional({
+//     description: 'The date when the debt was created',
+//     example: '2025-01-22',
+//   })
+//   @IsOptional()
+//   @IsDateString()
+//   debt_date?: string;
 
-  @ApiPropertyOptional({
-    description: 'The payment period for the debt',
-    enum: DebtPeriod,
-    example: DebtPeriod.MONTH3,
-  })
-  @IsOptional()
-  @IsEnum(DebtPeriod)
-  debt_period?: DebtPeriod;
+//   @ApiPropertyOptional({
+//     description: 'This is name',
+//   })
+//   debt_name?: string;
 
-  @ApiPropertyOptional({
-    description: 'The debt amount',
-    example: 1500.75,
-    type: 'number',
-  })
-  @IsOptional()
-  @IsDecimal({ decimal_digits: '2', force_decimal: false })
-  debt_sum?: number;
+//   @ApiPropertyOptional({
+//     description: 'The payment period for the debt',
+//     enum: DebtPeriod,
+//     example: DebtPeriod.MONTH3,
+//   })
+//   @IsOptional()
+//   @IsEnum(DebtPeriod)
+//   debt_period?: DebtPeriod;
 
-  @ApiPropertyOptional({
-    description: 'A description of the debt',
-    example: 'Debt for January 2025',
-  })
-  @IsOptional()
-  @IsString()
-  description?: string;
+//   @ApiPropertyOptional({
+//     description: 'The debt amount',
+//     example: 1500.75,
+//     type: 'number',
+//   })
+//   @IsOptional()
+//   @IsDecimal({ decimal_digits: '2', force_decimal: false })
+//   debt_sum?: number;
 
-  @ApiPropertyOptional({
-    description: 'The ID of the debtor',
-    example: 'debtor-12345',
-  })
-  @IsOptional()
-  debtor?: string;
-}
+//   @ApiPropertyOptional({
+//     description: 'A description of the debt',
+//     example: 'Debt for January 2025',
+//   })
+//   @IsOptional()
+//   @IsString()
+//   description?: string;
+
+//   @ApiPropertyOptional({
+//     description: 'The ID of the debtor',
+//     example: 'debtor-12345',
+//   })
+//   @IsOptional()
+//   debtor?: string;
+// }
+
+export class UpdateDebtDto extends PartialType(DebtDto) {}
